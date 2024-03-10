ebba540d4e51290839eec887d765938c6134e41e
Joris-G <66126635+Joris-G@users.noreply.github.com>
2024-03-11 00:43:39 +0200

    auth

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -1,24 +1,27 @@
 const userModel = require('../users/user.model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const authController = {
     async login(req, res) {
-        const { username, password } = req.body;
-        const user = userModel.getUserByUsername(username);
-
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid credentials' });
+        const { email, password } = req.body;
+        try {
+            console.log(email);
+            const user = await userModel.findOne({ where: { email } });
+            console.log(user);
+            if (user === null || !bcrypt.compareSync(password, user.password)) {
+                console.log(password);
+                return res.status(401).json({ message: 'Email ou mot de passe incorrect' });
+            }
+            // const token = jwt.sign({ userId: user.id, email: user.email }, 'votre_secret_key', { expiresIn: '10h' });
+            const token = jwt.sign({ email: user.email }, 'votre_secret_key', { expiresIn: '10h' });
+            console.log("Token : " + token);
+            res.json({ token, user: { id: user.id, email: user.email } });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Erreur interne du serveur' });
         }
-
-        const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (!passwordMatch) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        // Authenticated successfully
-        res.status(200).json({ message: 'Authentication successful' });
-    },
+    }
 };
 
 module.exports = authController;
