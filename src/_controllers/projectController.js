e6f47b19248921cba3b71d90c475d11d30c24dac
Joris-G <66126635+Joris-G@users.noreply.github.com>
2024-03-26 07:45:58 +0100

    project update

--- src/_controllers/projectController.js
+++ src/_controllers/projectController.js
@@ -1,51 +1,63 @@
-const Project = require('../_models/projects.model');
+const DB = require("../../database");
+const { ProjectError } = require("../_errors/customError");
+const moment = require("moment");
 
 const projectController = {
-    async create(req, res) {
-        const {
-            title,
-            description,
-            projectType,
-            startDate,
-            estimatedEndDate,
-            manager, status,
-            priority, assignedTo,
-            followers, dueDate,
-            createdBy, progressNotes } = req.body;
-        try {
-
-            const project = Project.create({
-                title, description,
-                projectType, startDate,
-                estimatedEndDate, manager,
-                status, priority,
-                assignedTo, followers,
-                dueDate, createdBy,
-                progressNotes
-            });
-            return res.status(201).json(project);
-        } catch (error) {
-            console.error(error);
-            return res.status(408).json("Erreur lors de la création du projet");
-        }
-    },
-
-    async getById(req, res) {
-        const id = req.params.id;
-        const project = Project.getById(id);
-
-        if (!project) {
-            return res.status(404).json({ message: 'Ce projet n\'existe pas' });
-        }
-        res.status(200).json(project);
-    },
-    async getAll(req, res) {
-        const projects = Project.getAll();
-        if (!projects) {
-            return res.status(404).json({ message: 'Aucune projet trouvé' });
-        }
-        res.status(200).json(projects);
+  async create(req, res, next) {
+    try {
+      const {
+        title,
+        description,
+        projectType,
+        startDate,
+        estimatedEndDate,
+        managerId,
+        status,
+        priority,
+      } = req.body;
+      const project = await DB.Project.create({
+        title,
+        description,
+        projectType,
+        startDate,
+        estimatedEndDate,
+        managerId,
+        status,
+        priority,
+      });
+      return res.status(201).json(project);
+    } catch (error) {
+      next();
     }
+  },
+
+  async getById(req, res) {
+    const id = req.params.id;
+    const project = Project.getById(id);
+
+    if (!project) {
+      return res.status(404).json({ message: "Ce projet n'existe pas" });
+    }
+    res.status(200).json(project);
+  },
+  async getAll(req, res, next) {
+    try {
+      const projects = await DB.Project.findAll({
+        include: {
+          model: DB.User,
+          as: "manager",
+          attributes: { exclude: ["password"] },
+        },
+      });
+      if (!projects) {
+        throw new ProjectError("Aucun projet trouvé");
+      }
+      console.log(projects);
+      res.status(200).json(projects);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
 module.exports = projectController;
--- src/_models/projects.model.js
+++ src/_models/projects.model.js
@@ -1,103 +1,104 @@
 const projets = [
-    {
-        id: 1,
-        nom: 'Projet A',
-        description: 'Développement d\'un nouvel avion de ligne à propulsion hybride.',
-        type: 'Build to Print',
-        dateDebut: new Date('2024-01-01'),
-        dateFinEstimee: new Date('2026-12-31'),
-        responsable: 'Ingénieur1',
-    },
-    {
-        id: 2,
-        nom: 'Projet B',
-        description: 'Conception et fabrication de composants composites pour un satellite en orbite géostationnaire.',
-        type: 'Build-to-Spec',
-        dateDebut: new Date('2024-03-15'),
-        dateFinEstimee: new Date('2025-11-30'),
-        responsable: 'Ingénieur2',
-    },
-    {
-        id: 3,
-        nom: 'Projet C',
-        description: 'Amélioration des processus de fabrication pour réduire les temps de cycle des pièces aéronautiques.',
-        type: 'Build to Print',
-        dateDebut: new Date('2024-05-01'),
-        dateFinEstimee: new Date('2024-12-31'),
-        responsable: 'Ingénieur3',
-    },
+  {
+    id: 1,
+    nom: "Projet A",
+    description:
+      "Développement d'un nouvel avion de ligne à propulsion hybride.",
+    type: "Build to Print",
+    dateDebut: new Date("2024-01-01"),
+    dateFinEstimee: new Date("2026-12-31"),
+    responsable: "Ingénieur1",
+  },
+  {
+    id: 2,
+    nom: "Projet B",
+    description:
+      "Conception et fabrication de composants composites pour un satellite en orbite géostationnaire.",
+    type: "Build-to-Spec",
+    dateDebut: new Date("2024-03-15"),
+    dateFinEstimee: new Date("2025-11-30"),
+    responsable: "Ingénieur2",
+  },
+  {
+    id: 3,
+    nom: "Projet C",
+    description:
+      "Amélioration des processus de fabrication pour réduire les temps de cycle des pièces aéronautiques.",
+    type: "Build to Print",
+    dateDebut: new Date("2024-05-01"),
+    dateFinEstimee: new Date("2024-12-31"),
+    responsable: "Ingénieur3",
+  },
 ];
 
-const { DataTypes } = require('sequelize');
-
-
+const { DataTypes } = require("sequelize");
 
 // const Task = require('./tasks.model');
 // const Document = require('./documents.model');
 // const User = require('./user.model');
 
 module.exports = (sequelize) => {
-    return sequelize.define('Project', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        projectType: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        startDate: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        estimatedEndDate: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        // manager: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false
-        // },
-        status: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        priority: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        assignedTo: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        followers: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        dueDate: {
-            type: DataTypes.DATEONLY,
-            allowNull: true,
-
-        },
-        // createdBy: {
-        //     type: DataTypes.STRING,
-        //     allowNull: true
-        // },
-        progressNotes: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        }
-    }, { timestamps: true });
-}
-
-
-
+  return sequelize.define(
+    "Project",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      projectType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      startDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      estimatedEndDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      // manager: {
+      //     type: DataTypes.STRING,
+      //     allowNull: false
+      // },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      priority: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      assignedTo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      followers: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      dueDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+      },
+      // createdBy: {
+      //     type: DataTypes.STRING,
+      //     allowNull: true
+      // },
+      progressNotes: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    },
+    { timestamps: true }
+  );
+};
